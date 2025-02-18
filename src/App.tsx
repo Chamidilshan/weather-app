c955a1a54cc7c03e3575a1643773f2f79fa47a9d
Chamika Dilshan <chamidilshan55@gmail.com>
2025-02-18 09:05:10 +0530

    started project and configured shadcn and tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='p-16'>Hello world</h1>
+    <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
