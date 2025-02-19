96ef3de3c364058decaa9f315d329bbd512ef849
Chamika Dilshan <chamidilshan55@gmail.com>
2025-02-19 08:37:58 +0530

    added weather api class with apis

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { Layout } from './components/layout'
 import './App.css'
-import { Button } from './components/ui/button'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ThemeProvider } from './components/themeprovider'
+import WeatherDashboard from './pages/weather-dashboard'
+import CityPage from './pages/city-page'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient();
 function App() {
-
   return (
-    <>
-    <h1 className='p-16'>Hello world</h1>
-    <Button>Click</Button>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<WeatherDashboard />} />
+              <Route path='/city/:cityName' element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+
+
   )
 }
 
